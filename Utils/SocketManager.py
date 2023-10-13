24d381dc4ac661cf12e1d68fb90f2392589b7ccb
Simon Lou <simon.lou.dev@proton.me>
2023-10-13 10:03:16 +0200

    Working on BL

--- Utils/SocketManager.py
+++ Utils/SocketManager.py
@@ -1,4 +1,9 @@
-import socket, subprocess, time, threading, json, eel
+import eel
+import json
+import socket
+import subprocess
+import threading
+import time
 
 bl = False
 
@@ -29,6 +34,7 @@ class SocketManager:
         print("Waiting for ConnCallBack")
         self.__WaitForConn() #Attente de la réponse du serveur
 
+    # Convertit la command en un string JSON pour l'envoyer
     def __CommandToJSON(self, command):
         x = {"command": command}
         return json.dumps(x)
@@ -53,7 +59,6 @@ class SocketManager:
         port = 5000  # socket server port number
         self.currentSocket = socket.socket() #Création du socket
         self.currentSocket.connect((host, port)) #Connexion au serveur
-        
     
     #Methode privée pour attendre la réponse du serveur
     def __WaitForConn(self):
@@ -94,18 +99,20 @@ class SocketManager:
                 recived = True
                 break
 
-    #Mettre dans une boucle
+    #Fonction qui permet de vérifier létat de la connextion toutes les 120 secondes avec un timeout de 30Sec
     def __startPingPong(self):
         time_start = time.time()
         launched = False
         launchedTime = time.time()
         if time.time() - time_start >= 120:  # Si le temps d'attente est dépassé, la connexion est échouée
-            self.__CommandToJSON("ping")
+            json = self.__CommandToJSON("ping")
+            self.currentSocket.send(json.encode())
             launched = True
             launchedTime = time.time()
 
         if launched and time.time() - launchedTime >= 30:
             print("timeout")
+            self.connIsOk = False
 
         try:
             data = self.currentSocket.recv(2048)
@@ -121,10 +128,85 @@ class SocketManager:
 
         if recive["command"] == "pong":  # Si la réponse est positive, la connexion est établie
             launched = False
-
+            self.connIsOk = True
 
     #Methode pemettant de savoir si la connexion est établie
     def getConState(self):
         return self.connIsOk
-        
 
+    # Methode permettant d'envoyer les données de calibrage
+    def configure(self, debit, pression):
+        data = {"command": "calibrate", "debit": debit, "pression": pression}
+        jsonStr = json.dumps(data)
+        self.currentSocket.send(jsonStr.encode())
+        self.__waitACK()
+
+    # Methode permmettant de changer la fréquence de l'ampli
+    def changeFrequency(self, freq):
+        data = {"command": "changeFreq", "frequency": freq}
+        jsonStr = json.dumps(data)
+        self.currentSocket.send(jsonStr.encode())
+        self.__waitACK()
+
+    # Methode permettant de démarer une mesure
+    def startMesure(self):
+        jsonStr = self.__CommandToJSON("startMesure")
+        self.currentSocket.send(jsonStr.encode())
+        self.__waitACK()
+        self.__readMesureData()
+
+    def stopMesure(self):
+        jsonStr = self.__CommandToJSON("stopMesure")
+        self.currentSocket.send(jsonStr.encode())
+        self.__waitACK()
+
+    # Methode permettant de lire les données de mesures et de les envoyer à son Model, (renvoi un ACK)
+    def __readMesureData(selfs):
+        stopped = False
+        time_start = time.time()
+        while not stopped:
+            recive = None
+            try:
+                data = self.currentSocket.recv(2048)
+                recive = data.decode()
+            except:
+                recive = ""
+
+            if len(recive) == 0:
+                continue
+
+            try:
+                recive = json.loads(recive)
+            except Exception as e:
+                print(e)
+
+            if recive["command"] == "sendData":  # Si la commande est send  data je lis sinon tant pis
+                if recive["ended"] == True: stopped = True  # Si c'est la dernière mesure qui est envoyé
+
+                print(recive["data"])
+                # Données dans recive["data"]
+
+    # Methode permettant d'attendre la confirmation de lecture
+    # TODO : Rajouter un Timeout et mettre la connexion hors line
+    def __waitACK(self):
+        ackRecive = False
+
+        while not ackRecive:
+            recive = None
+            try:
+                data = self.currentSocket.recv(2048)
+                recive = data.decode()
+            except:
+                recive = ""
+
+            if len(recive) == 0:
+                continue
+
+            try:
+                recive = json.loads(recive)
+            except Exception as e:
+                print(e)
+
+            if recive["command"] == "recive:ACK":  # Si la commande est l'acknoledgement je ferme la boucle et je quitte
+                ackRecive = True
+                return
