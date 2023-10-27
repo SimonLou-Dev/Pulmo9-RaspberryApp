9bae25dbaa5d4eaae5a46c0ee0ea13f1b2fae99b
Simon Lou <simon.lou.dev@proton.me>
2023-10-27 09:24:37 +0200

    Exclude DB

---
+++ Pulmo9-FrontEnd/Views/Calibration/Calibration.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Calibration = (props) => {
+
+
+        return (
+            <div className={"Calibration"}>
+                
+            </div>
+        )
+}
