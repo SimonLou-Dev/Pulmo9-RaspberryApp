b3101d34461c2614755954d29298247c1bad27a1
Simon Lou <simon.lou.dev@proton.me>
2023-11-22 11:10:03 +0100

    Mesuring and updates

---
+++ Pulmo9-FrontEnd/Views/Mesure/MesureData.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MesureData = (props) => {
+
+
+        return (
+            <div className={"MesureData"}>
+                
+            </div>
+        )
+}
---
+++ Pulmo9-FrontEnd/Views/Mesure/MesurePrepare.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MesurePrepare = (props) => {
+
+
+        return (
+            <div className={"MesurePrepare"}>
+                
+            </div>
+        )
+}
